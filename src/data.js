c50a7b75cd455849167c5b209b4d103954f4a11c
Phone Myint Thaw <pthaw423@gmail.com>
2022-10-31 11:40:58 +0630

    React Portfolio

---
+++ src/data.js
@@ -0,0 +1,100 @@
+export const projects = [
+    {
+        title: "Blog",
+        subtitle: "Laravel",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur itaque sunt, reiciendis eum totam minima accusamus omnis excepturi blanditiis dolorem incidunt optio eius repellat, quibusdam, voluptatum accusantium voluptatibus molestiae ab.",
+        image: "./laravelBlog.png",
+        link: "http://reactbootcamp.com",
+    },
+    {
+        title: "PHP User Management",
+        subtitle: "PHP",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur itaque sunt, reiciendis eum totam minima accusamus omnis excepturi blanditiis dolorem incidunt optio eius repellat, quibusdam, voluptatum accusantium voluptatibus molestiae ab.",
+        image: "./phpUserManagement.png",
+        link: "http://reactbootcamp.com",
+    },
+    {
+        title: "Student Management",
+        subtitle: "JavaScript and Local Storage",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur itaque sunt, reiciendis eum totam minima accusamus omnis excepturi blanditiis dolorem incidunt optio eius repellat, quibusdam, voluptatum accusantium voluptatibus molestiae ab.",
+        image: "./js studentManagement.png",
+        link: "http://reactbootcamp.com",
+    },
+    {
+        title: "Calculator",
+        subtitle: "Java Script",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur itaque sunt, reiciendis eum totam minima accusamus omnis excepturi blanditiis dolorem incidunt optio eius repellat, quibusdam, voluptatum accusantium voluptatibus molestiae ab.",
+        image: "./jsCalculator.png",
+        link: "http://reactbootcamp.com",
+    },
+    {
+        title: "Calculator",
+        subtitle: "Java Script",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur itaque sunt, reiciendis eum totam minima accusamus omnis excepturi blanditiis dolorem incidunt optio eius repellat, quibusdam, voluptatum accusantium voluptatibus molestiae ab.",
+        image: "./jsCalendar.png",
+        link: "http://reactbootcamp.com",
+    },
+    {
+        title: "Rock Paper Scissor ",
+        subtitle: "Java Script",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur itaque sunt, reiciendis eum totam minima accusamus omnis excepturi blanditiis dolorem incidunt optio eius repellat, quibusdam, voluptatum accusantium voluptatibus molestiae ab.",
+        image: "./jsRock.png",
+        link: "http://reactbootcamp.com",
+    },
+    {
+        title: "Shop API",
+        subtitle: "Java Script",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur itaque sunt, reiciendis eum totam minima accusamus omnis excepturi blanditiis dolorem incidunt optio eius repellat, quibusdam, voluptatum accusantium voluptatibus molestiae ab.",
+        image: "./jsshopapi.png",
+        link: "http://reactbootcamp.com",
+    },
+    {
+        title: "Todo",
+        subtitle: "Java Script",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur itaque sunt, reiciendis eum totam minima accusamus omnis excepturi blanditiis dolorem incidunt optio eius repellat, quibusdam, voluptatum accusantium voluptatibus molestiae ab.",
+        image: "./jstodo.png",
+        link: "http://reactbootcamp.com",
+    },
+    {
+        title: "CardCompoment",
+        subtitle: "Java Script",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur itaque sunt, reiciendis eum totam minima accusamus omnis excepturi blanditiis dolorem incidunt optio eius repellat, quibusdam, voluptatum accusantium voluptatibus molestiae ab.",
+        image: "./cardCompoment.png",
+        link: "http://reactbootcamp.com",
+    },
+    {
+        title: "Admin Dashboard UI",
+        subtitle: "Java Script",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur itaque sunt, reiciendis eum totam minima accusamus omnis excepturi blanditiis dolorem incidunt optio eius repellat, quibusdam, voluptatum accusantium voluptatibus molestiae ab.",
+        image: "./BoostarpDashboard.png",
+        link: "http://reactbootcamp.com",
+    },
+];
+
+export const testimonials = [
+    {
+        quote: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga accusamus incidunt aut doloremque tenetur! Consequuntur, soluta vitae porro a consectetur aut nulla vel nihil excepturi assumenda unde laudantium dignissimos animi.",
+        image: "./myphoto.jpg",
+        name: "Jasse Hicks",
+        company: "Zoozle",
+    },
+    {
+        quote: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga accusamus incidunt aut doloremque tenetur! Consequuntur, soluta vitae porro a consectetur aut nulla vel nihil excepturi assumenda unde laudantium dignissimos animi.",
+        image: "./myphoto.jpg",
+        name: "Jasse Hicks",
+        company: "Zoozle",
+    },
+];
+
+export const skills = [
+   "HTML",
+   "CSS",
+   "Bootstrap",
+   "Tailwind",
+   "JavaScript",
+   "React(Basic)",
+   "PHP",
+   "Laravel",
+   "MySQL",
+   "Git&GitHub",
+]
